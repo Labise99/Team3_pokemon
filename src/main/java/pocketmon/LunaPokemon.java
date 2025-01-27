d29125ac9d92022fb5da5754241ab6b7d9ee6e20
symoon521 <worldw@naver.com>
2025-01-27 11:05:38 +0900

    달맞이 동산 진화 포켓몬 추가

---
+++ src/main/java/pocketmon/LunaPokemon.java
@@ -0,0 +1,10 @@
+package pocketmon;
+
+import lombok.Getter;
+
+@Getter
+public class LunaPokemon extends Pokemon {
+    public LunaPokemon(String pokemonName, int HP, int level) {
+        super(pokemonName, HP, level);
+    }
+}
