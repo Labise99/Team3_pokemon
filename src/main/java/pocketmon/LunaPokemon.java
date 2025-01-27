f4cea399218826122368a172e9e7ed22e76653cf
symoon521 <worldw@naver.com>
2025-01-27 13:18:14 +0900

    Trainer 클래스 달맞이 동산 도착 시 삐삐나 푸린 소유중인지 검사
    LunaPokemon
    루나포켓몬 전용 진화로직 구현

--- src/main/java/pocketmon/LunaPokemon.java
+++ src/main/java/pocketmon/LunaPokemon.java
@@ -4,7 +4,23 @@ import lombok.Getter;
 
 @Getter
 public class LunaPokemon extends Pokemon {
+    private boolean evolved; // 진화 여부
+
     public LunaPokemon(String pokemonName, int HP, int level) {
         super(pokemonName, HP, level);
+        this.evolved = false;
+    }
+
+    // 진화 메서드
+    public EvolvedPokemon evolve() {
+        if (!evolved) { // 진화 여부 확인
+            this.evolved = true; // 진화 상태 업데이트
+            System.out.println("어? 어라 포켓몬이 이상하다...");
+            System.out.println(getPokemonName() + "이(가) 진화합니다!");
+            return new EvolvedPokemon(getPokemonName() + " (진화형)", getHP() + 20, getLevel());
+        } else {
+            System.out.println(getPokemonName() + "은(는) 이미 진화했습니다!");
+        }
+        return null;
     }
 }
