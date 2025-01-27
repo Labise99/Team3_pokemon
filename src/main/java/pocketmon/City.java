99ee6146f51353fdfc565b4f5fee7dcd3668dc5c
Labise99 <teenyrabbit@gmail.com>
2025-01-27 10:46:29 +0900

    cityName, connectedCities, constructor created

--- src/main/java/pocketmon/City.java
+++ src/main/java/pocketmon/City.java
@@ -1,6 +1,17 @@
 package pocketmon;
 
+import java.util.List;
+
 public class City {
     // 도시 정보 관련 클래스
-    // branch init
+    private String cityName; // 도시 이름
+    private List<City> connectedCities; // 연결된 도시 목록
+
+    // 생성자
+    public City(String cityName, List<City> connectedCities) {
+        this.cityName = cityName;
+        this.connectedCities = connectedCities;
+    }
+
+    // 그럼 이제 무슨 메서드가 필요할 것인가
 }
