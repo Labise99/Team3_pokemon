6be2bdc2263b0a37cca34f99df0662d60cbe74e5
Labise99 <teenyrabbit@gmail.com>
2025-01-27 11:32:28 +0900

    branch init

--- src/main/java/pocketmon/CityMap.java
+++ src/main/java/pocketmon/CityMap.java
@@ -2,4 +2,17 @@ package pocketmon;
 
 public class CityMap {
     // 지도 및 위치 이동 관련 클래스
+
+    // 도시 선언
+    City palletTown = new City("태초마을", null); // ?? : 태초마을이야!
+    City moonHill = new City("달맞이동산", null);
+
+    // 도시 이동 메소드
+    public void moveCity(Trainer trainer) {
+        // 현재 트레이너의 위치 정보 확인
+
+        // 이동 가능한 도시 목록 출력
+        // 이동할 도시 선택
+        // 선택한 도시로 이동
+    }
 }
